85b1d66cd04c06f5f5a5a12c102b4afaa1a960d8
AkShAY-Ca7 <136839322+AkShAY-Ca7@users.noreply.github.com>
2023-09-26 22:31:02 +0530

    UPDATED pass1.c and enterd conditions

--- pass1.c
+++ pass1.c
@@ -6,7 +6,7 @@ void main()
 {
     FILE *f1,*f2,*output,*symtab;
     int lc,sa,op1,o ,len;
-    char m1[20],la[20],op[20],otp[20];
+    char m1[20],la[20],op[20],otp[20],hex[20];
 
     f1=fopen("input.txt","r");
     f2=fopen("optab.txt","r");
@@ -30,10 +30,25 @@ void main()
         fscanf(f1,"%s\t%s\t%d",la,m1,&op1);
         if(strcmp(la,"**")==0)
         {
+            while(!feof(f2))
+            {
+                fscanf(f2,"%s\t%s\n",op,hex);
+                if(strcmp(m1,op)==0)
+                {
+                    lc=lc+3;
+                    fprintf(output,"%d\t%s\t%s\t%d\n",lc,la,m1,op1);
+                    break;
+                }
+
+            }
+
+        }
+        else if(strcmp(m1,"WORD"))
+        {
             lc=lc+3;
             fprintf(output,"%d\t%s\t%s\t%d\n",lc,la,m1,op1);
         }
-        else if(strcmp(m1,"WORD"))
+        else if(strcmp(m1,"RESW"))
         {
             
         }
