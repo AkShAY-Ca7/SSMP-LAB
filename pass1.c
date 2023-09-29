14c96ddfed3d5aa36c75cdb3fcdd627104e96892
Akshay C A <136839322+Akshay-C-A@users.noreply.github.com>
2023-09-29 12:00:29 +0530

    edited pass1.c needed to add byte section

--- pass1.c
+++ pass1.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 void main()
 {
@@ -19,34 +19,45 @@ void main()
     {
         sa=op1;
         lc=sa;
-        fprintf(output,"\t%s\t%s\t%d\n",la,m1,op1);
+        fprintf(output,"\tPRGM1\t%s\t%d\n",m1,op1);
     }
     else
     {
         lc=0;
     }
+    
+    fscanf(f1,"%s\t%s\t%s",la,m1,op2);
     while(strcmp(m1,"END")!=0)
     {
-        fscanf(f1,"%s\t%s\t%s",la,m1,op2);
+        //fscanf(f1,"%s\t%s\t%s",la,m1,op2);
         fprintf(output,"%d\t",lc);
-        if(strcmp(la,"**")==0)
+        //if( (strcmp(la,"**")==0) && (strcmp(m1,"END")!=0) )
+         if(strcmp(la,"**")==0)
         {
-          /*  while(!feof(f2))
-            {
-                fscanf(f2,"%s\t%s\n",op,hex);
-                if(strcmp(m1,op)==0)
-                {
-                    lc=lc+3;
-                    fprintf(output,"%d\t%s\t%s\t%d\n",lc,la,m1,op1);
-                    break;
-                }
-            }*/
-		lc=lc+3;
+        	int flag=0;
+        	while(!feof(f2))
+        	{	
+                	fscanf(f2,"%s\t%s\n",op,hex);
+                	if(strcmp(m1,op)==0)
+                	{
+                    		lc=lc+3;
+                    		flag=1;
+                    		break;
+                	}
+            	}
+            	if(flag==0)
+            	{
+            		printf("Opcode not found !!  ERROR !!\n Exiting....");
+            		exit(0);
+            	}
         }
+        
+        //else if((strcmp(la,"**")!=0))
         else
         {
         	fprintf(symtab,"%s\t%d\n",la,lc);
-        }	
+        }
+        
         if(strcmp(m1,"WORD")==0)
         {
             lc=lc+3;
@@ -60,5 +71,7 @@ void main()
             lc=lc+atoi(op2);
         }
         fprintf(output,"%s\t%s\t%s\n",la,m1,op2);
+        fscanf(f1,"%s\t%s\t%s",la,m1,op2);
     }
+    fprintf(output,"%d\t%s\t%s\t%d\n",lc,la,m1,sa);
 }
